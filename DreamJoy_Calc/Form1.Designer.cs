11aa3f490920230c49a3635d128f35887c284a41
Dries Maes <dm.inbox@outlook.com>
2021-01-18 15:38:28 +0100

    titel form aangepast

--- DreamJoy_Calc/Form1.Designer.cs
+++ DreamJoy_Calc/Form1.Designer.cs
@@ -208,7 +208,7 @@ namespace DreamJoy_Calc
             this.Controls.Add(this.buttonRekPlus);
             this.Controls.Add(this.TitleRek);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Rekening manager";
             ((System.ComponentModel.ISupportInitialize)(this.numRek)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.numSpaar)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.numZicht)).EndInit();
