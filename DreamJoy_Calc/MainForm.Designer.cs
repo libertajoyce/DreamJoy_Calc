5d8ec969ecc082638e5d247345624093f7b2256b
Dries Maes <dm.inbox@outlook.com>
2021-01-20 14:57:30 +0100

    Colorfull buttons!

--- DreamJoy_Calc/MainForm.Designer.cs
+++ DreamJoy_Calc/MainForm.Designer.cs
@@ -41,28 +41,45 @@ namespace DreamJoy_Calc
             // 
             // btnForm1
             // 
-            this.btnForm1.Location = new System.Drawing.Point(86, 121);
+            this.btnForm1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnForm1.FlatAppearance.BorderColor = System.Drawing.Color.Maroon;
+            this.btnForm1.FlatAppearance.BorderSize = 3;
+            this.btnForm1.FlatAppearance.CheckedBackColor = System.Drawing.Color.Maroon;
+            this.btnForm1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.IndianRed;
+            this.btnForm1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Maroon;
+            this.btnForm1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnForm1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnForm1.Location = new System.Drawing.Point(51, 74);
+            this.btnForm1.Margin = new System.Windows.Forms.Padding(2);
             this.btnForm1.Name = "btnForm1";
-            this.btnForm1.Size = new System.Drawing.Size(188, 58);
+            this.btnForm1.Size = new System.Drawing.Size(111, 46);
             this.btnForm1.TabIndex = 0;
             this.btnForm1.Text = "Rekening";
-            this.btnForm1.UseVisualStyleBackColor = true;
+            this.btnForm1.UseVisualStyleBackColor = false;
             this.btnForm1.Click += new System.EventHandler(this.btnForm1_Click);
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(72, 121);
+            this.button1.Location = new System.Drawing.Point(42, 74);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(8, 8);
+            this.button1.Size = new System.Drawing.Size(5, 5);
             this.button1.TabIndex = 1;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // btnForm2
             // 
-            this.btnForm2.Location = new System.Drawing.Point(86, 232);
+            this.btnForm2.FlatAppearance.BorderColor = System.Drawing.Color.Maroon;
+            this.btnForm2.FlatAppearance.BorderSize = 3;
+            this.btnForm2.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.btnForm2.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Maroon;
+            this.btnForm2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnForm2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnForm2.Location = new System.Drawing.Point(51, 194);
+            this.btnForm2.Margin = new System.Windows.Forms.Padding(2);
             this.btnForm2.Name = "btnForm2";
-            this.btnForm2.Size = new System.Drawing.Size(188, 58);
+            this.btnForm2.Size = new System.Drawing.Size(111, 45);
             this.btnForm2.TabIndex = 2;
             this.btnForm2.Text = "Dier";
             this.btnForm2.UseVisualStyleBackColor = true;
@@ -70,9 +87,16 @@ namespace DreamJoy_Calc
             // 
             // btnForm3
             // 
-            this.btnForm3.Location = new System.Drawing.Point(86, 343);
+            this.btnForm3.FlatAppearance.BorderColor = System.Drawing.Color.Maroon;
+            this.btnForm3.FlatAppearance.BorderSize = 3;
+            this.btnForm3.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.btnForm3.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Maroon;
+            this.btnForm3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnForm3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnForm3.Location = new System.Drawing.Point(51, 313);
+            this.btnForm3.Margin = new System.Windows.Forms.Padding(2);
             this.btnForm3.Name = "btnForm3";
-            this.btnForm3.Size = new System.Drawing.Size(188, 58);
+            this.btnForm3.Size = new System.Drawing.Size(111, 42);
             this.btnForm3.TabIndex = 3;
             this.btnForm3.Text = "Cilinder";
             this.btnForm3.UseVisualStyleBackColor = true;
@@ -84,16 +108,18 @@ namespace DreamJoy_Calc
             this.label1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.label1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(72, 24);
+            this.label1.Location = new System.Drawing.Point(42, 15);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(510, 41);
+            this.label1.Size = new System.Drawing.Size(310, 25);
             this.label1.TabIndex = 4;
             this.label1.Text = "Welcome to headquarters mr bond";
             // 
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(320, 121);
+            this.pictureBox1.Location = new System.Drawing.Point(233, 74);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(500, 282);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
@@ -102,16 +128,17 @@ namespace DreamJoy_Calc
             // 
             // MainForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(17F, 41F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ControlText;
-            this.ClientSize = new System.Drawing.Size(1098, 652);
+            this.ClientSize = new System.Drawing.Size(823, 464);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.btnForm3);
             this.Controls.Add(this.btnForm2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.btnForm1);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "MainForm";
             this.Text = "HQ";
             this.Load += new System.EventHandler(this.MainForm_Load);
